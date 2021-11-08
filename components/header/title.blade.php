8ccfd84ec1ec035415796c5ab9cccdc4c8d3c314
Tiago Papile <tiagopapile@gmail.com>
2021-11-08 18:09:59 -0300

    an-1 Corrige tag de título do site

--- components/header/title.blade.php
+++ components/header/title.blade.php
@@ -2,7 +2,7 @@
     <header class="container">
         <div class="row">
             <div class="col py-5 text-center text-xl-start">
-                <span class="pa-tag rounded-1 px-3 py-1 d-inline-block">Vídeos Adventistas</span>
+                <span class="pa-tag rounded-1 px-3 py-1 d-inline-block">{{ __('Adventist news', 'iasd') }}</span>
                 <h1 class="mt-2">@getHeaderTitle</h1>
             </div>
         </div>
