b3f221f98ed3a9f3709e5157bed2e02a992387e2
Tiago Papile <tiagopapile@gmail.com>
2021-11-08 12:56:36 -0300

    an-8 Ajusta mobile de single

--- components/header/title.blade.php
+++ components/header/title.blade.php
@@ -1,8 +1,8 @@
 <section class="pa-header py-3">
     <header class="container">
         <div class="row">
-            <div class="col py-5">
-                <span class="pa-tag rounded-1 px-3 py-1 d-table-cell">Vídeos Adventistas</span>
+            <div class="col py-5 text-center text-xl-start">
+                <span class="pa-tag rounded-1 px-3 py-1 d-inline-block">Vídeos Adventistas</span>
                 <h1 class="mt-2">@getHeaderTitle</h1>
             </div>
         </div>
