32f81c9a28d29ca61883443cbec2daf685278e06
Eli Mendonça <contato@elimendonca.com>
2021-11-18 19:53:46 -0300

    Ajuste nos tamanhos das imagens na resposta da API

--- functions.php
+++ functions.php
@@ -155,8 +155,8 @@ function featured_media_url_callback($post)
         'full'             => !empty($full    = wp_get_attachment_image_src($img_id, ''))             ? $full[0]    : '',
         'medium'           => !empty($medium  = wp_get_attachment_image_src($img_id, 'medium_large')) ? $medium[0]  : '',
         'small'            => !empty($small   = wp_get_attachment_image_src($img_id, 'thumbnail'))    ? $small[0]   : '',
-        'pa-block-preview' => !empty($preview = wp_get_attachment_image_src($img_id, 'medium_large')) ? $preview[0] : '',
-        'pa-block-render'  => !empty($render  = wp_get_attachment_image_src($img_id, 'medium_large')) ? $render[0]  : '',
+        'pa-block-preview' => !empty($preview = wp_get_attachment_image_src($img_id, 'thumbnail')) ? $preview[0] : '',
+        'pa-block-render'  => !empty($render  = wp_get_attachment_image_src($img_id, 'medium')) ? $render[0]  : '',
     );
 
     return $img_scr;
@@ -178,8 +178,8 @@ function avatar_callback($user)
         'full'             => !empty($full    = wp_get_attachment_image_src($img_id, ''))             ? $full[0]    : '',
         'medium'           => !empty($medium  = wp_get_attachment_image_src($img_id, 'medium_large')) ? $medium[0]  : '',
         'small'            => !empty($small   = wp_get_attachment_image_src($img_id, 'thumbnail'))    ? $small[0]   : '',
-        'pa-block-preview' => !empty($preview = wp_get_attachment_image_src($img_id, 'medium_large')) ? $preview[0] : '',
-        'pa-block-render'  => !empty($render  = wp_get_attachment_image_src($img_id, 'medium_large')) ? $render[0]  : '',
+        'pa-block-preview' => !empty($preview = wp_get_attachment_image_src($img_id, 'thumbnail')) ? $preview[0] : '',
+        'pa-block-render'  => !empty($render  = wp_get_attachment_image_src($img_id, 'medium')) ? $render[0]  : '',
     );
 
     return $img_scr;
